2132f10ccecb33a05e553469ac5c258e7c895b21
anura1402 <ann99909@gmail.com>
2024-10-24 02:09:18 +0300

    Simple GET to get one product

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -49,4 +52,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation ("com.squareup.picasso:picasso:2.8")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.7.2")
+    implementation ("com.squareup.okhttp3:okhttp:4.7.2")
 }
---
+++ app/src/main/java/ru/anura/retrofittraining/retrofit/ProductApi.kt
@@ -0,0 +1,9 @@
+package ru.anura.retrofittraining.retrofit
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ProductApi {
+    @GET("products/{id}")
+    suspend fun getProductById(@Path("id") id: Int): Product
+}
